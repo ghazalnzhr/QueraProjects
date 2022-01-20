27ad2d95143c3dff8facd88cedc8fe3ce3fdbee5
ghazalnzhr <96284770+ghazalnzhr@users.noreply.github.com>
2022-01-20 12:15:56 +0330

    finished TimerBomb

--- TimerBomb.ino
+++ TimerBomb.ino
@@ -5,12 +5,11 @@ https://wokwi.com/arduino/projects/320920656704176724
 */
 
 // Countdown variable in minutes
-unsigned long baseTime = 2; // Timer is set to two minutes
+unsigned long baseTime = 1; // Timer is set to two minutes
 
 /* Variables and constants */
 // LCD Setup
 #include <LiquidCrystal.h>
-
 LiquidCrystal lcd(12, 11, 10, 9, 8, 7);
 
 /* LED Setup */
@@ -29,6 +28,9 @@ const int threeYellow = 15;
 const int oneGreen = 16;
 const int twoGreen = 17;
 const int threeGreen = 18;
+
+int leds[10] = {18, 17, 16, 15, 14, 13, 5, 4, 3, 2};
+
 
 // Time Constants
 unsigned long int secondsPerMinute = 60;
@@ -115,61 +117,39 @@ void printTimeToLcd(unsigned long hours, unsigned long minutes, unsigned long se
 /* LED Management */
 // Gradually light up LEDs until the time is up
 void ledManager(double percentage) {
-  double precentLeft = double(1) - percentage;
-  Serial.println(precentLeft);
-  if (precentLeft >= (double) 0.1) {
-    digitalWrite(oneYellow, HIGH);
-  } else if (precentLeft >= (double) 0.2) {
-    digitalWrite(oneRed, HIGH);
-  } else if (precentLeft >= (double) 0.3) {
-    digitalWrite(oneGreen, HIGH);
-  } else if (precentLeft >= (double) 0.4) {
-    digitalWrite(threeYellow, HIGH);
-  } else if (precentLeft >= (double) 0.5) {
-    digitalWrite(twoYellow, HIGH);
-  } else if (precentLeft >= (double) 0.6) {
-    digitalWrite(oneYellow, HIGH);
-  } else if (precentLeft >= (double) 0.7) {
-    digitalWrite(oneRed, HIGH);
-  } else if (precentLeft >= (double) 0.8) {
-    digitalWrite(twoRed, HIGH);
-  } else if (precentLeft >= (double) 0.9) {
-    digitalWrite(threeRed, HIGH);
-  } else if (precentLeft >= (double) 1) {
-    digitalWrite(fourRed, HIGH);
+
+  double percentPassed = double(1) - percentage;
+  percentPassed *= (double)10;
+  percentPassed = int(percentPassed);
+  Serial.println(percentPassed);
+  for (int i=0; i<percentPassed; i++) {
+    /* Serial.println("led");
+    Serial.println(i); */
+    digitalWrite(leds[i], HIGH);
+    //Serial.println(leds[i]);
   }
 }
 
+
 void setup() {
   lcd.begin(16, 2);
-  pinMode(buzzerPin, OUTPUT);
-  pinMode(oneRed, OUTPUT);
-  pinMode(twoRed, OUTPUT);
-  pinMode(threeRed, OUTPUT);
-  pinMode(fourRed, OUTPUT);
-  pinMode(oneYellow, OUTPUT);
-  pinMode(twoYellow, OUTPUT);
-  pinMode(threeYellow, OUTPUT);
-  pinMode(oneGreen, OUTPUT);
-  pinMode(twoGreen, OUTPUT);
-  pinMode(oneGreen, OUTPUT);
-  /* for (int jj=14; jj<19;jj++){
-    pinMode(jj,OUTPUT);
+  
+  for (int i=0; i<11;i++){
+    pinMode(leds[i],OUTPUT);
     }
-    for (int ii=2; ii<6;ii++){
-    pinMode(ii,OUTPUT);
-    } */
   Serial.begin(9600);
-
 }
 
 void loop() {
+  // Calculate time in seconds
+  totalTime = minutesToSeconds(baseTime);
   // Caculates the delta between the total time and the elapsed time in seconds
-  timeLeft = minutesToSeconds(baseTime) - (millis() / 1000);
+
+  timeLeft = totalTime - (millis() / 1000);
 
   // Save the first instance of the time left variable for the lcd before changing it
   // for convertion to hh:mm:ss
-  totalTime = timeLeft;
+
   // Is the timer over?
   if (timeLeft == 0) {
     // handle the start of the timer
@@ -178,7 +158,7 @@ void loop() {
       firstTime = false;
     } else {
       tone(buzzerPin, 1000); // Send 1KHz sound signal...
-      delay(1000); // ...for 1 sec
+      delay(1000);        // ...for 1 sec
       noTone(buzzerPin);
       lcd.clear();
       lcd.print("Time is over");
@@ -204,8 +184,9 @@ void loop() {
     delay(1000);
     // After one second, print the new value
     lcd.clear();
-    delta = ((double) totalTime - (double) millis() / 1000) / (double) 120;
+    delta = ((double)totalTime - (double)millis() / 1000) / (double)totalTime;
     //delta /= totalTime;
     ledManager(delta);
   }
 }
+
